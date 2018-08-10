8d1aed3a48af5881bc5269fed88784a4893557e0
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-08-10 09:31:21 -0700

    init

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "google" {}
+
+module "dcos-tested-oses" {
+  source  = "dcos-terraform/gcp-tested-oses/template"
+  version = "~> 0.0"
+
+  providers = {
+    google = "google"
+  }
+
+  os = "${var.dcos_instance_os}"
+}
+
+module "dcos-pubagt-instances" {
+  source  = "dcos-terraform/instance/gcp"
+  version = "~> 0.0"
+
+  providers = {
+    google = "google"
+  }
+
+  cluster_name             = "${var.cluster_name}"
+  hostname_format          = "${var.hostname_format}"
+  num_instances            = "${var.num_public_agents}"
+  image                    = "${coalesce(var.image, module.dcos-tested-oses.gcp_image_name)}"
+  user_data                = "${var.image == "" ? module.dcos-tested-oses.os-setup : var.user_data}"
+  machine_type             = "${var.machine_type}"
+  instance_subnetwork_name = ["${var.public_agent_subnetwork_name}"]
+  ssh_user                 = "${var.ssh_user}"
+  public_ssh_key           = "${var.public_ssh_key}"
+  zone_list                = "${var.zone_list}"
+  disk_type                = "${var.disk_type}"
+  disk_size                = "${var.disk_size}"
+  tags                     = "${var.tags}"
+  region                   = "${var.region}"
+}
