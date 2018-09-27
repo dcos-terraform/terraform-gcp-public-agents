f61fcd2ba364b8a675c26045f3fb2e352b81f2db
Lukas Loesche <lukas@mesosphere.io>
2018-09-27 16:55:59 +0200

    Automatic update of variable descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,84 +1,101 @@
 # Number of Instance
 output "num_public_agents" {
+  description = "Specify the amount of public agents. These agents will host marathon-lb and edgelb"
   value = "${var.num_public_agents}"
 }
 
 # Cluster Name
 output "name_prefix" {
+  description = "Cluster Name"
   value = "${var.name_prefix}"
 }
 
 # Instance Type
 output "machine_type" {
+  description = "machine type"
   value = "${var.machine_type}"
 }
 
 # Element by zone list
 output "zone_list" {
+  description = "zone list"
   value = "${var.zone_list}"
 }
 
 # Source image to boot from
 output "image" {
+  description = "image"
   value = "${var.image}"
 }
 
 # Disk Type to Leverage
 output "disk_type" {
+  description = "Disk Type to Leverage. The GCE disk type. Can be either 'pd-ssd', 'local-ssd', or 'pd-standard'. (optional)"
   value = "${var.disk_type}"
 }
 
 # Disk Size in GB
 output "disk_size" {
+  description = "disk size"
   value = "${var.disk_size}"
 }
 
 # Instance Subnetwork Name
 output "public_agent_subnetwork_name" {
+  description = "public agent subnetwork name"
   value = "${var.public_agent_subnetwork_name}"
 }
 
 # Customer Provided Userdata
 output "user_data" {
+  description = "User data to be used on these instances (cloud-init)"
   value = "${var.user_data}"
 }
 
 # SSH User
 output "ssh_user" {
+  description = "ssh user"
   value = "${module.dcos-public-agent-instances.ssh_user}"
 }
 
 # SSH Public Key
 output "public_ssh_key" {
+  description = "public ssh key"
   value = "${var.public_ssh_key}"
 }
 
 # Private IP Addresses
 output "private_ips" {
+  description = "List of private ip addresses created by this module"
   value = ["${module.dcos-public-agent-instances.private_ips}"]
 }
 
 # Public IP Addresses
 output "public_ips" {
+  description = "List of public ip addresses created by this module"
   value = ["${module.dcos-public-agent-instances.public_ips}"]
 }
 
 # Tested DCOS OSes Name
 output "dcos_instance_os" {
+  description = "Operating system to use. Instead of using your own AMI you could use a provided OS."
   value = "${var.dcos_instance_os}"
 }
 
 # Preemptible Scheduling (bool)
 output "scheduling_preemptible" {
+  description = "scheduling preemptible"
   value = "${var.scheduling_preemptible}"
 }
 
 # Public Node Self Link
 output "instances_self_link" {
+  description = "List of instance self links"
   value = ["${module.dcos-public-agent-instances.instances_self_link}"]
 }
 
 # Returns the ID of the prereq script (if images are not used)
 output "prereq_id" {
+  description = "prereq id"
   value = "${module.dcos-public-agent-instances.prereq_id}"
 }
--- variables.tf
+++ variables.tf
@@ -1,61 +1,87 @@
 # Number of Instance
-variable "num_public_agents" {}
+variable "num_public_agents" {
+  description = "Specify the amount of public agents. These agents will host marathon-lb and edgelb"
+}
 
 # Cluster Name
-variable "name_prefix" {}
+variable "name_prefix" {
+  description = "Cluster Name"
+}
 
 # Instance Type
-variable "machine_type" {}
+variable "machine_type" {
+  description = "machine type"
+}
 
 # DCOS version
-variable "dcos_version" {}
+variable "dcos_version" {
+  description = "Specifies which DC/OS version instruction to use. Options: 1.9.0, 1.8.8, etc. See dcos_download_path or dcos_version tree for a full list."
+}
 
 # Element by zone list
 variable "zone_list" {
+  description = "zone list"
   type    = "list"
   default = []
 }
 
 # Source image to boot from
-variable "image" {}
+variable "image" {
+  description = "image"
+}
 
 # Disk Type to Leverage
-variable "disk_type" {}
+variable "disk_type" {
+  description = "Disk Type to Leverage. The GCE disk type. Can be either 'pd-ssd', 'local-ssd', or 'pd-standard'. (optional)"
+}
 
 # Disk Size in GB
-variable "disk_size" {}
+variable "disk_size" {
+  description = "disk size"
+}
 
 # Instance Subnetwork Name
-variable "public_agent_subnetwork_name" {}
+variable "public_agent_subnetwork_name" {
+  description = "public agent subnetwork name"
+}
 
 # Customer Provided Userdata
 variable "user_data" {
+  description = "User data to be used on these instances (cloud-init)"
   default = ""
 }
 
 # SSH User
-variable "ssh_user" {}
+variable "ssh_user" {
+  description = "ssh user"
+}
 
 # SSH Public Key
-variable "public_ssh_key" {}
+variable "public_ssh_key" {
+  description = "public ssh key"
+}
 
 # Add special tags to the resources created by this module
 variable "tags" {
+  description = "Add custom tags to all resources"
   type    = "list"
   default = []
 }
 
 # Deploy instance with preemptible scheduling. (bool)
 variable "scheduling_preemptible" {
+  description = "scheduling preemptible"
   default = "false"
 }
 
 # Format the hostname inputs are index+1, region, name_prefix
 variable "hostname_format" {
+  description = "Format the hostname inputs are index+1, region, cluster_name"
   default = "%[2]s-pubagts-%[1]d"
 }
 
 # The operating system to use. Instead of using your own AMI you could use a provided OS.
 variable "dcos_instance_os" {
+  description = "Operating system to use. Instead of using your own AMI you could use a provided OS."
   default = "centos_7.4"
 }
