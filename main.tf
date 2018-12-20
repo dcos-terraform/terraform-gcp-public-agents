c955122322f27deedb0465c630783d1cce83a6e2
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-12-20 14:32:33 -0800

    main/readme: updating version from 0.0.0 to 0.1.0

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@
  *```hcl
  * module "pubagts" {
  *   source = "dcos-terraform/instances/gcp"
- *   version = "~> 0.0.0"
+ *   version = "~> 0.1.0"
  *
  *   num_instance                   = "${var.instances_count}"
  *   disk_size                      = "${var.gcp_instances_disk_size}"
@@ -35,7 +35,7 @@ locals {
 
 module "dcos-public-agent-instances" {
   source  = "dcos-terraform/instance/gcp"
-  version = "~> 0.0.0"
+  version = "~> 0.1.0"
 
   providers = {
     google = "google"
